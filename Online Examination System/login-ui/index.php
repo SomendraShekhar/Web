8320f52ba28ba9812feead761fde4399ae822029
SomendraShekhar <56575449+SomendraShekhar@users.noreply.github.com>
2021-06-01 16:11:55 +0530

    Add files via upload

---
+++ Online Examination System/login-ui/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Rajkiya Engineering College Banda</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="login-ui/image/png" href="images/icons/favicon.ico"/>
+	
+	<link rel="stylesheet" type="text/css" href="login-ui/css/util.css">
+	<link rel="stylesheet" type="text/css" href="login-ui/css/main.css">
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form-title" style="background-image: url(logo.png);">
+					<span class="login100-form-title-1">
+						Sign In
+					</span>
+				</div>
+
+				<form method="post" id="examineeLoginFrm" class="login100-form validate-form">
+					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="text" name="username" placeholder="Enter email">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="pass" placeholder="Enter password">
+						<span class="focus-input100"></span>
+					</div>
+
+
+					<div class="container-login100-form-btn" align="right">
+						<button type="submit" class="login100-form-btn">
+							Login
+						</button>
+					</div>
+					<a href="registration.php"> not registered? click here
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	<script src="login-ui/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="login-ui/vendor/animsition/js/animsition.min.js"></script>
+	<script src="login-ui/vendor/bootstrap/js/popper.js"></script>
+	<script src="login-ui/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="login-ui/vendor/select2/select2.min.js"></script>
+	<script src="login-ui/vendor/daterangepicker/moment.min.js"></script>
+	<script src="login-ui/vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="login-ui/vendor/countdowntime/countdowntime.js"></script>
+	<script src="login-ui/js/main.js"></script>
+
+</body>
+</html>
